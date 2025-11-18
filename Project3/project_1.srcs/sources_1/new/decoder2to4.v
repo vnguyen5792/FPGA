3b7e9d5d0fd244f06bc4c456394cf2df9a9be85c
Viet Nguyen <vietaddisonguyen@gmail.com>
2025-11-18 11:57:54 -0600

    hello

--- Project3/project_1.srcs/sources_1/new/decoder2to4.v
+++ Project3/project_1.srcs/sources_1/new/decoder2to4.v
@@ -3,6 +3,11 @@ output reg [3:0] an);
 always@(en) begin
 
 // use if else statement
+if (en == 2'b00) an <= 4'b1000;
+else if (en == 2'b01) an <= 4'b0100;
+else if (en == 2'b10) an <= 4'b0010;
+else if (en == 2'b11) an <= 4'b0001;
+else an <= 4'b0000; 
 
 end
 endmodule
--- Project3/project_1.srcs/sources_1/new/mux4to1.v
+++ Project3/project_1.srcs/sources_1/new/mux4to1.v
@@ -3,8 +3,16 @@ input [3:0] B,
 input [3:0] C,
 input [3:0] D,
 input [1:0] sel, 
-output [3:0] Y);
+output reg [3:0] Y);
 
-
+always@(sel)
+begin
 //use case statement
+    case(sel)
+        2'b00: Y <= A;
+        2'b01: Y <= B;
+        2'b10: Y <= C;
+        2'b11: Y <= D;
+    endcase 
+end
 endmodule
